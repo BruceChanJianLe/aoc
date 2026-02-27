16ab9a07a3f6b18bf0eadbe1700e5d26cc5b3aff
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-02-27 23:31:11 +0800

    wip: a place holder for day7 part1

---
+++ 2025/day7/cpp/day7_part1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "input_reader.hpp"
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+  static const char* default_input = "test";
+
+  std::string_view input_path;
+  if (argc != 2) {
+    input_path = default_input;
+  } else if (argc == 2){
+    input_path = argv[1];
+  }
+
+  auto inputs = aoc::InputReader::get_input<std::string>(input_path);
+
+  return 0;
+}
